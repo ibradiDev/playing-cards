cd45793415c7d129deb89fa2d76b08b745c105c4
ibradiDev <ibradi.dev@gmail.com>
2025-01-15 15:31:54 +0000

    "Routes" implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,16 @@ import {PlayingCardComponent} from './components/playing-card/playing-card.compo
 import {Monster} from './models/monster.model';
 import {SearchBarComponent} from './components/search-bar/search-bar.component';
 import {MonsterService} from './services/monster/monster.service';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayingCardComponent, SearchBarComponent,],
+  imports: [PlayingCardComponent, SearchBarComponent, RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   // title = 'playing-cards';
-  monsterService = inject(MonsterService);
 
-  monsters = signal<Monster[]>([]);
-  searchText = model("");
-
-  filteredMonsters = computed(() => {
-    return this.monsters().filter(monster => monster.name.includes(this.searchText()));
-  })
-
-  selectedMonsterIndex = signal(1);
-  selectedMonster = computed(() => {
-    return this.monsters()[this.selectedMonsterIndex()];
-  });
-
-  constructor() {
-    this.monsters.set(this.monsterService.getAll());
-  }
-
-  addGenericMonster(): void {
-    const genericMonster = new Monster();
-    this.monsterService.add(genericMonster);
-    this.monsters.set(this.monsterService.getAll());
-  }
 }
